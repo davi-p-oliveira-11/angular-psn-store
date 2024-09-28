4170970290d042baf43774fb1f956eaf9e3006bb
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-28 00:04:32 -0300

    added project first version

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-pricing.component.html',
+  styleUrl: './card-pricing.component.css',
+})
+export class CardPricingComponent {
+  @Input()
+  gameType: string = 'Digital PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
+  @Input()
+  gameCover: string = '';
+  @Input()
+  gameLabel: string = '';
+}
